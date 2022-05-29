bb79f4d4964957d7d2b488f03e84341f4d52c3d9
Eduardo Aquino <edutraquino@gmail.com>
2022-05-29 11:15:21 -0300

    refact: move render fn to another file

--- js/index.js
+++ js/index.js
@@ -1,42 +1,11 @@
-import list from "./mock.js";
-const listEl = document.getElementById("list");
+import config from "../config.js";
+import apiService from "./api-service.js";
+import renderMovie from "./util/render-movie.js";
 
-/**
- *
- * @param {{image: string; title: string; rating: number; year: number; description: string; isFavorited: boolean;}} movie
- * @returns
- */
-function renderMovie(movie) {
-  const card = document.createElement("div");
-  card.className = "card shadow rounded grid";
-  card.innerHTML = `
-<div class="left">
-    <img
-    class="full-rounded"
-    src=${movie.image}
-    alt="Imagem do poster do filme"
-    />
-</div>
-<div class="info">
-    <h2>${movie.title} (${movie.year})</h2>
-    <span><img
-        id="rating"
-        src="./static/star.svg"
-        alt="Estrela representando o rating do filme"
-    />${movie.rating}</span>
-    <span><img
-        id="heart"
-        src="./static/heart-off.svg"
-        alt="Coração representando se o filme está nos favoritos"
-    />Favoritar</span>
-</div>
-<div class="description">
-    <p>
-    ${movie.description}
-    </p>
-</div>
-`;
-  listEl.appendChild(card);
-  return card;
-}
-list.forEach(renderMovie);
+const listEl = document.getElementById("list");
+const renderMovieInList = renderMovie.bind(this, listEl);
+
+const service = apiService(config.API_KEY);
+service.getPopularMovies().then((results) => {
+  results.forEach(renderMovieInList);
+});
