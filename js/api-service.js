cafe1d52f60c0b9d962fcdb3b298beb517aab536
Eduardo Aquino <edutraquino@gmail.com>
2022-05-29 11:16:14 -0300

    feat: create service to request data from API

---
+++ js/api-service.js
@@ -0,0 +1,31 @@
+import parseToMovie from "./util/parse-to-movie.js";
+/**
+ * This function create a service to request data to MovieDB API.
+ * @param {string} apiKey
+ */
+const apiService = (apiKey) => {
+  const api = `?api_key=${apiKey}`;
+  const url = "https://api.themoviedb.org/3/movie";
+
+  return {
+    getPopularMovies: async () => {
+      const data = await fetch(`${url}/popular/${api}`);
+      const results = await data.json();
+      const arr = results.results.map(parseToMovie);
+      return arr;
+    },
+
+    /**
+     * This function returns the data about the requested movie
+     * @param {string | number} movieId
+     * @returns
+     */
+    getMovie: async (movieId) => {
+      const data = await fetch(`${url}/${movieId}/${api}`);
+      const movie = await data.json();
+      return parseToMovie(movie);
+    },
+  };
+};
+
+export default apiService;
