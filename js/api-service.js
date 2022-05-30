7d44e81f42866df1deb6f929792a733d5cf397db
Eduardo Aquino <edutraquino@gmail.com>
2022-05-30 14:01:41 -0300

    refact: api service converted to class

--- js/api-service.js
+++ js/api-service.js
@@ -1,44 +1,49 @@
+import Movie from "./model/Movie.js";
 import parseToMovie from "./util/parse-to-movie.js";
 /**
  * This function create a service to request data to MovieDB API.
  * @param {string} apiKey
  */
-const apiService = (apiKey) => {
-  const api = `?api_key=${apiKey}`;
-  const url = "https://api.themoviedb.org/3";
+class ApiService {
+  constructor(apiKey) {
+    this.api = `?api_key=${apiKey}`;
+    this.url = "https://api.themoviedb.org/3";
+  }
 
-  return {
-    getPopularMovies: async () => {
-      const data = await fetch(`${url}/movie/popular/${api}`);
-      const results = (await data.json()).results;
-      const arr = results.map(parseToMovie);
-      return arr;
-    },
+  /**
+   * This function returns a list with all popular movies in the database
+   * @returns {Movie[]} list of movies
+   */
+  async getPopularMovies() {
+    const data = await fetch(`${this.url}/movie/popular/${this.api}`);
+    const results = (await data.json()).results;
+    const arr = results.map(parseToMovie);
+    return arr;
+  }
 
-    /**
-     * This function returns the data about the requested movie
-     * @param {string | number} movieId
-     * @returns
-     */
-    getMovie: async (movieId) => {
-      const data = await fetch(`${url}/movie/${movieId}/${api}`);
-      const movie = await data.json();
-      return parseToMovie(movie);
-    },
+  /**
+   * This function returns the data about the requested movie
+   * @param {string | number} movieId
+   * @returns {Movie} movie data
+   */
+  async getMovie(movieId) {
+    const data = await fetch(`${this.url}/movie/${movieId}/${this.api}`);
+    const movie = await data.json();
+    return parseToMovie(movie);
+  }
 
-    /**
-     * This function sends a text query to the API and returns a movie list
-     * @param {string} query
-     * @returns
-     */
-    searchMovie: async (query) => {
-      query = `&query=${query}`;
-      const data = await fetch(`${url}/search/movie${api}${query}`);
-      const results = (await data.json()).results;
-      const arr = results.map(parseToMovie);
-      return arr;
-    },
-  };
-};
+  /**
+   * This function sends a text query to the API and returns a movie list
+   * @param {string} query
+   * @returns {Movie[]} list of movies
+   */
+  async searchMovie(query) {
+    query = `&query=${query}`;
+    const data = await fetch(`${this.url}/search/movie${this.api}${query}`);
+    const results = (await data.json()).results;
+    const arr = results.map(parseToMovie);
+    return arr;
+  }
+}
 
-export default apiService;
+export default ApiService;
--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 import config from "../config.js";
-import apiService from "./api-service.js";
+import ApiService from "./api-service.js";
 import ListUpdater from "./list-updater.js";
 
 const listEl = document.getElementById("list");
 const listUpdater = new ListUpdater(listEl);
 
-const service = apiService(config.API_KEY);
+const service = new ApiService(config.API_KEY);
 service.getPopularMovies().then((results) => listUpdater.setMovies(results));
 
 // Form actions
