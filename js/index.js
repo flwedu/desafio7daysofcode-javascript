9fe23e3549895d7b6e8f66ab43a49dff8ca1628c
Eduardo Aquino <edutraquino@gmail.com>
2022-05-30 20:36:23 -0300

    feat: show favorite/all movies button working

--- js/index.js
+++ js/index.js
@@ -2,13 +2,27 @@ import config from "../config.js";
 import ApiService from "./api-service.js";
 import ListUpdater from "./list-updater.js";
 
+const favoritesCheckbox = document.getElementById("favorites");
 const listEl = document.getElementById("list");
 const listUpdater = new ListUpdater(listEl);
+let allMovies = [];
 
 const service = new ApiService(config.API_KEY);
-service.getPopularMovies().then((results) => listUpdater.setMovies(results));
+service.getPopularMovies().then((results) => {
+  allMovies = results;
+  listUpdater.setMovies(allMovies);
+});
 
-// Form actions
+// Show only favorites movies
+favoritesCheckbox.addEventListener("change", () => {
+  if (favoritesCheckbox.checked)
+    return listUpdater.setMovies(
+      allMovies.filter((movie) => movie.isFavorited === true)
+    );
+  return listUpdater.setMovies(allMovies);
+});
+
+// Search form
 const form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
