e43ad964681c6ca4db8a558a4875e50a1038e13d
Eduardo Aquino <edutraquino@gmail.com>
2022-05-31 09:42:24 -0300

    feat: show favorited movies of last search

--- js/index.js
+++ js/index.js
@@ -9,27 +9,35 @@ const storage = new StorageService("favorites_movies_db");
 const favoritesCheckbox = document.getElementById("favorites");
 const listEl = document.getElementById("list");
 const listUpdater = new ListUpdater(listEl, storage);
-let allMovies = [];
+let popularMovies = [];
+let searchedMovies = [];
 
 service.getPopularMovies().then((results) => {
-  allMovies = results;
-  listUpdater.setMovies(allMovies);
+  popularMovies = results;
+  searchedMovies = results;
+  listUpdater.setMovies(popularMovies);
 });
 
 // Show only favorites movies
 favoritesCheckbox.addEventListener("change", () => {
   if (favoritesCheckbox.checked)
     return listUpdater.setMovies(
-      allMovies.filter((movie) => movie.isFavorited === true)
+      searchedMovies.filter((movie) => movie.isFavorited === true)
     );
-  return listUpdater.setMovies(allMovies);
+  return listUpdater.setMovies(searchedMovies);
 });
 
 // Search form
 const form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  service
-    .searchMovie(form["query"].value)
-    .then((results) => listUpdater.setMovies(results));
+  const query = form["query"].value;
+  if (query) {
+    return service.searchMovie(form["query"].value).then((results) => {
+      searchedMovies = results;
+      listUpdater.setMovies(searchedMovies);
+    });
+  }
+  searchedMovies = popularMovies;
+  return listUpdater.setMovies(searchedMovies);
 });
